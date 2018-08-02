38025573e11fdc7b9a184a2d8a56374e74aa3edf
Phuc Thai Minh <phuctm@zigexn.vn>
2018-08-02 17:27:49 +0700

    temp commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,19 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      
+    else
+      render 'new'
+    end
+  end
 end
