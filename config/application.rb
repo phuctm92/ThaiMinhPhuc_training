afdff75f4c31ccb0cefe152bcaee4753d0b5c6e9
Phuc Thai Minh <phuctm@zigexn.vn>
2018-08-15 17:17:22 +0700

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
+require "action_cable/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -26,5 +27,8 @@ module ThaiMinhPhucTraining
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
